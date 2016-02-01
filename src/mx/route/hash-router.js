e7d42b45ed72c799058449d96c27679213c693c6
Henry Li <henry.li03@sap.com>
2016-02-01 15:46:54 +0800

    mx.goto(path, [query], [replace])

--- src/mx/route/hash-router.js
+++ src/mx/route/hash-router.js
@@ -60,21 +60,62 @@ export default class HashRouter extends Router
         }
     }
 
-    goto(path, query = {}, replace = false)
+    goto(path, query, replace)
     {
+        if (arguments.length === 0)
+        {
+            throw new Error("mx.goto() requires at least 1 argument.");
+        }
+        else if (arguments.length === 2)
+        {
+            if (typeof(arguments[1]) === "boolean")
+            {
+                replace = arguments[1];
+                query = undefined;
+            }
+        }
+
+        if (typeof(replace) === "undefined" || replace === null)
+        {
+            replace = false;
+        }
+
         if (path[0] !== "/")
         {
             throw new Error("The path of navigation request must start with '/'.");
         }
 
-        let queryStrings = [];
-        for (let key in query)
+        if (typeof(query) !== "undefined" && query !== null)
         {
-            queryStrings.push(key + "=" + encodeURIComponent(query[key]));
-        }
-        if (queryStrings.length)
-        {
-            path += "?" + queryStrings.join("&");
+            let queryString = null;
+            if (typeof(query) === "string")
+            {
+                if (query.length > 0)
+                {
+                    queryString = encodeURIComponent(query);
+                }
+            }
+            else if (jQuery.isPlainObject(query))
+            {
+                let queryStrings = [];
+                for (let key in query)
+                {
+                    queryStrings.push(key + "=" + encodeURIComponent(query[key]));
+                }
+                if (queryStrings.length > 0)
+                {
+                    queryString = queryStrings.join("&");
+                }
+            }
+            else
+            {
+                throw new Error("The query argument of mx.goto() must be a string or a plain object.");
+            }
+
+            if (queryString !== null && queryString.length > 0)
+            {
+                path += "?" + queryString;
+            }
         }
 
         if (this._path === path)
